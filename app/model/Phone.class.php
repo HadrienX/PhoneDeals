adc8766ae3a4982063856c8c1a920619490f34c0
Hadrien <hadriien@live.fr>
2016-02-14 11:29:03 +0100

    Update Phone.class.php
    Ajout de la méthode getPhoneThumbnail qui permet de récupérer la première image du téléphone pour l'afficher comme image de miniature.
    Si l'image est introuvable, on affiche une image par défaut

--- app/model/Phone.class.php
+++ app/model/Phone.class.php
@@ -21,7 +21,7 @@
 		}
 
 		public function __set($n, $v){
-			$this->$n=$v;
+			$this->$n = $v;
 		}
 
 		public function __toString(){
@@ -50,6 +50,27 @@
 			$sth->execute();
 			
 			return $sth->fetchAll();
+		}
+
+		public static function getPhoneThumbnail($id) {
+			PDOConnexion::setParameters('phonedeals', 'root', 'root');
+			$db = PDOConnexion::getInstance();
+			$sql = 'SELECT name, color FROM phone WHERE id = :id';
+			$sth = $db->prepare($sql);
+			$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Phone');
+			$sth->execute(array(
+				':id' => $id
+			));
+			
+			$req = $sth->fetch();
+			$colors = explode(',', $req->color);
+			$imgUrl = 'uploads/phones/' . $id . '/' . App::url($req->name) . '-' . $colors[0] . '.jpg';
+
+			if (!file_exists($imgUrl)) {
+				return 'img/default-thumbnail.jpg';
+			}
+
+			return $imgUrl;
 		}
 	}
 ?>
