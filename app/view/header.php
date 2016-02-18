3ab2125179b19f428cac03e98f8a026575e3dee8
ValGou <val.gougeon@hotmail.fr>
2016-02-18 22:08:24 +0100

    Update header.php

--- app/view/header.php
+++ app/view/header.php
@@ -9,6 +9,8 @@
 		<title><?php echo App::$siteTitle; ?></title>
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
