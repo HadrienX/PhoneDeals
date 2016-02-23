7cc5cc50ed96fef41dd34bf495ba5621e14a461f
Hadrien <hadriien@live.fr>
2016-02-23 09:17:10 +0100

    Update profile.php
    Ajout de la méthode simplifié de header

--- app/view/profile.php
+++ app/view/profile.php
@@ -2,16 +2,36 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
+				<div class="row" style="margin-bottom: 15px;">
+					<div class="col-md-12">
+						<h1>Votre profil</h1>
+					</div>
+				</div>
 				<div class="row">
-					<h1>Votre profil</h1>
-					<img src="<?php echo App::getGravatar($member->email); ?>" alt="Avatar de <?php echo $member->first_name; ?>" />
+					<div class="col-md-3">
+						<img src="<?php echo App::getGravatar($member->email); ?>" alt="Avatar de <?php echo $member->first_name; ?>" />
+					</div>
+					<div class="col-md-9">
+						<h3>Email</h3>
+						<?php echo $member->email; ?>
+						<h3>Adresse</h3>
+						<?php echo $member->way_num; ?>
+						<?php echo $member->way_type; ?>
+						<?php echo $member->way_name; ?><br />
+						<?php echo $member->zip_code; ?>
+						<?php echo $member->city; ?>
+						<h3>Inscrit depuis le</h3>
+						<?php echo $member->register_date; ?><br />
+						<a href="#" class="btn btn-lg btn-primary" style="margin-top: 30px;">Éditer</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 <?php
 	}
+
 	else {
-		header("HTTP/1.0 404 Not Found");
+		App::getHeader(404);
 	}
 ?>
