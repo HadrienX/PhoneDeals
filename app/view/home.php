f3d85ae91ff18effee85b0bad884d6875339b0e5
Hadrien <hadriien@live.fr>
2016-02-23 22:36:14 +0100

    Update home.php
    Ajout des promotions

--- app/view/home.php
+++ app/view/home.php
@@ -5,13 +5,25 @@
 			<div class="row">
 				<?php
 					foreach (Phone::getPhonesList() as $phone) {
+						if (Promotion::getPromotionByPhoneId($phone->id)) {
+							$phone->promotionPrice = $phone->price - ((Promotion::getPromotionByPhoneId($phone->id)->percent) * 0.01) * $phone->price;
+						}
+
 						echo '<div class="col-md-4 col-sm-6 col-xs-12" style="text-align: center">';
 							echo '<div style="border: 1px solid #ededed; padding: 30px; margin: 20px 0;">';
 								echo '<a href="index.php?page=product&id=' . $phone->id . '">';
 									echo '<img src="' . Phone::getPhoneThumbnail($phone->id) . '" alt="' . $phone->name . '" style="width: 100px;">';
 									echo '<h4>' . $phone->name . '</h4>';
 								echo '</a>';
-								echo '<span class="badge">' . $phone->price . ' &euro;</span>';
+								echo '<span class="badge">';
+									if (isset($phone->promotionPrice)) {
+										echo $phone->promotionPrice . ' &euro; au lieu de ' . $phone->price;
+									}
+
+									else {
+										echo $phone->price;
+									}
+								echo '&euro;</span>';
 							echo '</div>';
 						echo '</div>';
 					}
@@ -21,7 +33,22 @@
 
 		<div class="col-md-4">
 			<h2>Promotions</h2>
-			<p>Aucune promotion en ce moment</p>
+			<?php
+				if (Promotion::getPromotionsList()) {
+					echo '<div class="list-group">';
+						foreach (Promotion::getPromotionsList() as $promotion) {
+							echo '<a href="index.php?page=product&id=' . $promotion->phone . '" class="list-group-item">';
+								echo Phone::getPhoneById($promotion->phone)->name;
+								echo ' <span style="background-color: #e85142;" class="badge">- ' . $promotion->percent . ' %</span>';
+							echo '</a>';
+						}
+					echo '</div>';
+				}
+
+				else {
+					echo '<p>Aucune promotion en ce moment</p>';
+				}
+			?>
 		</div>
 	</div>
 </div>
