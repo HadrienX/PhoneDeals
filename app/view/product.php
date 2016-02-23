bc48d1cb3b51a34d1a2bbeae47ad21e0bdb39a33
Hadrien <hadriien@live.fr>
2016-02-23 08:15:22 +0100

    Update product.php
    Correction d'erreurs JS gênant la dernière couleur, et clarification du code pour une meilleure compréhension

--- app/view/product.php
+++ app/view/product.php
@@ -1,54 +1,66 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<?php
-					if (!isset($_GET['id'])) {
-						echo "Vous n'avez pas entré d'ID";
-					}
-					else {
-						$id = $_GET['id'];
-						$p = Phone::getPhoneById($id);
-						echo "<h1 style='text-align:center;''>{$p->name}</h1>";
-						echo '<div class="col-md-4 col-sm-6 col-xs-12" style="text-align: center; border: 1px solid #ededed; padding: 15px;">';
-							
-							echo '<span>';
-							$colors = explode(",", $p->color);
-							foreach($colors as $color){
-								$c = Color::getColorById($color);
-								echo '<div class="color" style="width:20px; height:20px; float:right; margin-right:5px; background-color:' . $c->hex . '; border:solid 1px silver;" onmouseover="this.style.cursor=\'pointer\';" onmouseout="this.style.cursor=\'\';"" value="' . $c->id . '"></div>';
-							}
-							echo '</span>';
-							echo '<a href="index.php?page=product&id=' . $p->id . '">';
-								echo '<img id="phone_img" src="' . Phone::getPhoneThumbnail($p->id) . '" alt="' . $p->name . '" style="width: 250px;">';
-							echo '</a>';
-						echo '</div>';
-						echo '<div class="col-md-4 col-sm-6 col-xs-12" style="border: 1px solid #ededed; padding: 15px;">';
-						$b = Brand::getBrandById($p->brand);
-						echo '<p>Marque : ' . $b->brand_name . '</p>';
-						foreach($p as $e) {
-							echo "<p>$e</p>";
-						}
-						echo '</div>';
-						echo '<div class="col-md-4 col-sm-6 col-xs-12" style="text-align: center; border: 1px solid #ededed; padding: 15px;">';
-						echo '<span style="font-weight:bolder; font-size:3em;">' . $p->price . ' &euro;</span></br>';
-						echo '<span>Quantité : <select>';
-						for($i=1;$i<11;$i++){
-							echo "<option>$i</option>";
-						}
-						echo '</select></span></br>';
-						$capacities = explode(",", $p->capacity);
-						echo '<span style="position:relative; top:10px;">Capacité : <select>';
-						foreach($capacities as $e){
-							echo "<option>$e</option>";
-						}
-						echo '</select></span></br>';
-						echo '<button type="submit" class="btn btn-default btn-lg" style="margin-top:30px;">Acheter</button>';
-						echo '</div>';
-					}
-				?>
+		<?php
+			if (!isset($_GET['id'])) {
+				echo 'Vous n\'avez pas entré d\'ID';
+			}
+			
+			else {
+				$id = $_GET['id'];
+				$phone = Phone::getPhoneById($id);
+		?>
+			<div class="col-sm-12" style="margin-bottom: 30px;">
+				<h1 style="margin-bottom: 0;"><?php echo $phone->name; ?></h1>
+				<?php $b = Brand::getBrandById($phone->brand); ?>
+				<span style="color: #bfbfbf; display: block; font-size: 18px;"><?php echo $b->brand_name; ?></span>
 			</div>
-		</div>
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<a href="index.php?page=product&amp;id=<?php echo $phone->id; ?>" style="display: inline-block; padding: 15px; border: 1px solid #ededed;">
+					<img id="phone_img" src="<?php echo Phone::getPhoneThumbnail($phone->id); ?>" alt="<?php echo $phone->name; ?>" style="width: 300px;">
+				</a>
+				<div id="phone-colors" style="text-align: center; margin-top: 15px;">
+					<?php
+						$colors = explode(',', $phone->color);
+						
+						foreach($colors as $color) {
+							$c = Color::getColorById($color);
+							echo '<div class="color" style="width:20px; height:20px; display: inline-block; border-radius: 20px; margin-right:5px; background-color:' . $c->hex . '; border:solid 1px silver;" onmouseover="this.style.cursor=\'pointer\';" onmouseout="this.style.cursor=\'\';" value="' . $c->id . '" title="' . $c->name . '"></div>';
+						}
+					?>
+				</div>
+			</div>
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<span style="font-weight:bolder; font-size:3em;"><?php echo $phone->price; ?> &euro;</span><br />
+				<div id="amount">
+					Quantité :
+					<select>
+						<?php
+							for ($i = 1; $i < 11; $i++) {
+								echo '<option>' . $i . '</option>';
+							}
+						?>
+					</select>
+				</div>
+				<div id="capacity" style="margin-top: 10px;">
+					<?php $capacities = explode(',', $phone->capacity); ?>
+					Capacité :
+					<select>
+						<?php
+							foreach($capacities as $e) {
+								echo '<option value="' . $e . '">' . $e . ' Go</option>';
+							}
+						?>
+					</select>
+				</div>
+				<button type="submit" class="btn btn-primary btn-lg" style="margin-top:30px;">Acheter</button>
+			</div>
+			<div class="col-md-12">
+				<h3>Description</h3>
+				<p><?php echo $phone->description; ?></p>
+			</div>
+		<?php
+			}
+		?>
 	</div>
 	<script type="text/javascript">
 		$(document).ready(function() {
