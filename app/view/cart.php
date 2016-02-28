3a20ca44b5fe990ccab4c2647faecd705413bdb2
Hadrien <hadriien@live.fr>
2016-02-28 18:35:08 +0100

    Update cart.php
    Début d'un panier, fonctionnel mais à améliorer

--- app/view/cart.php
+++ app/view/cart.php
@@ -1,3 +1,57 @@
+<?php
+	if (isset($_POST['action'])) {
+		if ($_POST['action'] == 'add') {
+			$phone = Phone::getPhoneById($_POST['id']);
+
+			// Si la session existe, alors on initialise la valeur dans une variable
+			if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
+				$cart = $_SESSION['cart'];
+			}
+
+			// Sinon, on initialise simplement la variable vide
+			else {
+				$cart = array(
+					'phones' => array(),
+					'phone_total' => (int) 0,
+					'price_total' => (int) 0,
+				);
+			}
+
+			// Lorsqu'on ajoute un téléphone, on le stocke dans un tableau
+			array_push($cart['phones'], array(
+				'id' => (int) $phone->id,
+				'name' => $phone->name,
+				'price' => (int) $phone->price
+			));
+
+			// On précise que les données entrés précédement correspondent à la variable $cart pour les ajouter dans la session
+			$_SESSION['cart'] = $cart;
+
+			$_SESSION['cart']['phone_total'] = $_SESSION['cart']['phone_total'] + 1;
+			$_SESSION['cart']['price_total'] = $_SESSION['cart']['price_total'] + $phone->price;
+
+			echo count($_SESSION['cart']['price_total']);
+		}
+
+		elseif ($_POST['action'] == 'remove') {
+			unset($_SESSION['cart']['phones'][$_POST['id']]);
+		}
+
+		else {
+			echo 'Erreur';
+		}
+	}
+
+	else {
+		$empty = true;
+		$phonesTotal = 0;
+
+		if (isset($_SESSION['cart']['phones']) && !empty($_SESSION['cart']['phones'])) {
+			$empty = false;
+			$phonesTotal = $_SESSION['cart']['phone_total'];
+		}
+?>
+
 <div class="container">
 	<h1>Panier d'achats</h1>
 
@@ -6,19 +60,42 @@
 			<table class="table">
 				<thead>
 					<tr>
-						<th>Produits ajoutés au panier (0)</th>
-						<th>Prix à l'unité</th>
-						<th>Quantité</th>
+						<th>Produits ajoutés au panier (<?php echo $phonesTotal; ?>)</th>
+						<th>Prix</th>
+						<th>Couleur</th>
+						<th>Capacité</th>
 						<th>Total</th>
 						<th>Supprimer</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>Vous n'avez ajouté aucun produit au panier</td>
-					</tr>
+					<?php
+						if (!$empty) {
+							foreach ($_SESSION['cart']['phones'] as $phone) {
+								echo '
+									<tr>
+										<td>' . $phone['name'] . '</td>
+										<td>' . money_format('%!i', $phone['price']) . '</td>
+										<td></td>
+										<td></td>
+										<td>' . money_format('%!i', $phone['price']) . '</td>
+										<td><a href="#" data-action="remove-cart" data-id="' . $phone['id'] . '" data-price="' . $phone['price'] . '" title="Retirer du panier"><i class="fa fa-trash"></i></a></td>
+									</tr>
+								';
+							}
+						}
+
+						else {
+							echo '
+								<tr>
+									<td>Vous n\'avez ajouté aucun produit au panier</td>
+								</tr>
+							';
+						}
+					?>
 				</tbody>
 			</table>
 		</div>
 	</div>
 </div>
+<?php } ?>
