87b38019bb0dc7fdf778e0285cd11fd3c42aa9ab
ValGou <val.gougeon@hotmail.fr>
2016-03-08 11:12:28 +0100

    Update signup.php

--- app/view/signup.php
+++ app/view/signup.php
@@ -10,8 +10,8 @@
     	   	isset($_POST['password-confirm']) && $_POST['password-confirm']==$_POST['password'] &&
     	   	isset($_POST['way_num'])&& $_POST['way_num']!="" && preg_match("#^[0-9]{1,}$#", $_POST['way_num']) &&
        		isset($_POST['way_type']) && $_POST['way_type']!="" &&
-       		isset($_POST['way_name'])&& $_POST['way_name']!="" && preg_match("#[a-zA-Z]{2,30}#", $_POST['way_name']) &&
-       		isset($_POST['city'])&& $_POST['city']!="" && preg_match("#[a-zA-Z]{2,30}#", $_POST['city']) &&
+       		isset($_POST['way_name'])&& $_POST['way_name']!="" && preg_match("#^[a-zA-Z0-9._-]{2,30}#", $_POST['way_name']) &&
+       		isset($_POST['city'])&& $_POST['city']!="" && preg_match("#^[a-zA-Z0-9._-]{2,30}#", $_POST['city']) &&
        		isset($_POST['zip_code']) && $_POST['zip_code']!="" && preg_match("#^[0-9]{5}$#", $_POST['zip_code']) &&
     	   	isset($_POST['accept_terms'])){
     	    
@@ -27,13 +27,13 @@
     	    
     	    
 			try{
-				PDOConnexion::setParameters('stages', 'root', 'root');
+				PDOConnexion::setParameters('phonedeals', 'root', 'root');
 				$db = PDOConnexion::getInstance();
 				$sql = "
-					INSERT INTO student(`first_name`, `last_name`, `email`, `password`, `way_num`, `way_type`, `way_name`, `city`, `zip_code`)
+					INSERT INTO member(`first_name`, `last_name`, `email`, `password`, `way_num`, `way_type`, `way_name`, `city`, `zip_code`)
 							VALUES (:first_name, :last_name, :email, :password, :way_num, :way_type, :way_name, :city, :zip_code)";
 				$sth = $db->prepare($sql);
-				$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Student');
+				$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Member');
 				$sth->execute(array(
 					':first_name' => $first_name,
 					':last_name' => $last_name,
@@ -86,10 +86,10 @@
 			if(!preg_match("#^[0-9]{1,}$#", $_POST['way_num'])){
 				App::error("Veuillez entrer un numéro de voie approprié");
 			}
-			if(!preg_match("#[a-zA-Z]{2,30}#", $_POST['way_name'])){
+			if(!preg_match("#^[a-zA-Z0-9._-]{2,30}#", $_POST['way_name'])){
 				App::error("Veuillez entrer un nom de voie approprié");
 			}
-			if(!preg_match("#^[a-zA-Z]{2,32}$#", $_POST['city'])){
+			if(!preg_match("#^[a-zA-Z0-9._-]{2,30}#", $_POST['city'])){
 				App::error("Veuillez entrer une ville approprié");
 			}
 			if(!preg_match("#^[0-9]{5}$#", $_POST['zip_code'])){
@@ -123,52 +123,52 @@
 				<div class="row">
 					<div class="col-md-6">
 						<label for="signup-firstname">Prénom</label>
-						<input type="text" name="first_name" class="form-control" id="signup-firstname" placeholder="Prénom">
+						<input type="text" name="first_name" class="form-control" required="required" id="signup-firstname" placeholder="Prénom">
 					</div>
 
 					<div class="col-md-6">
 						<label for="signup-lastname">Nom</label>
-						<input type="text" name="last_name" class="form-control" id="signup-lastname" placeholder="Nom">
+						<input type="text" name="last_name" class="form-control" required="required" id="signup-lastname" placeholder="Nom">
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-md-12">
 						<label for="signup-email">Adresse email</label>
-						<input type="text" name="email" class="form-control" id="signup-email" placeholder="Adresse email">
+						<input type="text" name="email" class="form-control" required="required" id="signup-email" placeholder="Adresse email">
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-md-12">
 						<label for="signup-email-confirm">Confirmez votre adresse email</label>
-						<input type="text" name="email-confirm" class="form-control" id="signup-email-confirm" placeholder="Confirmez votre adresse email">
+						<input type="text" name="email-confirm" class="form-control" required="required" id="signup-email-confirm" placeholder="Confirmez votre adresse email">
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-md-12">
 						<label for="signup-email">Mot de passe</label>
-						<input type="password" name="password" class="form-control" id="signup-password" placeholder="Mot de passe">
+						<input type="password" name="password" class="form-control" required="required" id="signup-password" placeholder="Mot de passe">
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-md-12">
 						<label for="signup-email-confirm">Confirmez votre mot de passe</label>
-						<input type="password" name="password-confirm" class="form-control" id="signup-password-confirm" placeholder="Confirmez votre mot de passe">
+						<input type="password" name="password-confirm" class="form-control" required="required" id="signup-password-confirm" placeholder="Confirmez votre mot de passe">
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-md-6">
 						<label for="signup-waynum">Numéro de voie</label>
-						<input type="text" name="way_num" class="form-control" id="signup-waynum" placeholder="Numéro de voie">
+						<input type="text" name="way_num" class="form-control" required="required" id="signup-waynum" placeholder="Numéro de voie">
 					</div>
 
 					<div class="col-md-6">
 						<label for="signup-waytype">Type de voie</label>
-						<select name="way_type" id="signup-waytype" class="form-control">
+						<select name="way_type" id="signup-waytype" required="required" class="form-control">
 							<option value=""></option>
 							<?php
 								foreach (Member::getWayTypes() as $way) {
@@ -184,19 +184,19 @@
 				<div class="row">
 					<div class="col-md-12">
 						<label for="signup-wayname">Adresse</label>
-						<input type="text" name="way_name" class="form-control" id="signup-wayname" placeholder="Adresse">
+						<input type="text" name="way_name" class="form-control" required="required" id="signup-wayname" placeholder="Adresse">
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col-md-6">
 						<label for="signup-city">Ville</label>
-						<input type="text" name="city" class="form-control" id="signup-city" placeholder="Ville">
+						<input type="text" name="city" class="form-control" required="required" id="signup-city" placeholder="Ville">
 					</div>
 
 					<div class="col-md-6">
 						<label for="signup-zipcode">Code postal</label>
-						<input type="text" name="zip_code" class="form-control" id="signup-zipcode" placeholder="Code postal">
+						<input type="text" name="zip_code" class="form-control" required="required" id="signup-zipcode" placeholder="Code postal">
 					</div>
 				</div>
 
@@ -206,7 +206,7 @@
 						<textarea class="form-control">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at quam eget dolor mattis dapibus sit amet in ante. Nunc mattis euismod dapibus. Nunc ullamcorper finibus metus dapibus tristique. Maecenas malesuada bibendum metus, quis lacinia nisl consectetur in. In nec ex ac nulla sagittis tempus sed eu sem. Maecenas ac tortor eu justo volutpat tincidunt. Etiam commodo magna consequat risus pharetra, at interdum velit congue. Morbi sed leo congue, placerat libero ut, pretium lorem. Fusce maximus nisl eu lectus lacinia malesuada. Ut placerat eget odio eget commodo. Duis venenatis tincidunt blandit. Ut vitae hendrerit massa. Donec elementum semper dolor quis varius. Nam sollicitudin lacus vel elit porttitor mollis.</textarea>
 						<div class="checkbox">
 							<label>
-								<input type="checkbox" name="accept_terms" id="signup-terms">
+								<input type="checkbox" name="accept_terms" required="required" id="signup-terms">
 								J'ai lu et j'accepte les <a href="#">conditions d'utilisations</a> et la <a href="#">politique de confidentialité</a>
 							</label>
 						</div>
