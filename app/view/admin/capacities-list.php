c94aba93adbcce180d3670c40ed0ec5160b6219e
ValGou <val.gougeon@hotmail.fr>
2016-03-15 12:55:15 +0100

    Update capacities-list.php

--- app/view/admin/capacities-list.php
+++ app/view/admin/capacities-list.php
@@ -9,14 +9,13 @@
 		</thead>
 		<?php
 			foreach (Capacity::getCapacityList() as $capacity) {
-				echo '<tr>';
-					echo '<td>' . $capacity->id . '</td>';
+				echo '<tr data-id="'. $capacity->id . '">';
 					echo '<td>' . $capacity->storage . ' Go</td>';
 					echo '<td><a href="index.php?page=admin/capacity-edit&amp;id=' . $capacity->id . '"><i class="fa fa-pencil" data-toggle="tooltip" title="Modifier"></i></a></td>';
-					echo '<td><a href="#"><i class="fa fa-trash" data-toggle="tooltip" title="Supprimer"></i></a></td>';
+					echo '<td><a href="#" title="Supprimer" data-action="delete" data-toggle="tooltip" title="Supprimer"><i class="fa fa-trash"></i></a></td>';
 				echo '</tr>';
 			}
 		?>
 	</table>
-	<a href="index.php?page=admin/add-capacity" class="btn btn-primary">Ajouter une capacité</a>
+	<a href="index.php?page=admin/add-capacity" class="btn btn-primary">Ajouter une capcité</a>
 </div>
