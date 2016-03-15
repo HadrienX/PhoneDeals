3fe062ba7af825aa43156809d61d65d297e07b5d
ValGou <val.gougeon@hotmail.fr>
2016-03-15 11:57:42 +0100

    Update brands-list.php

--- app/view/admin/brands-list.php
+++ app/view/admin/brands-list.php
@@ -9,13 +9,47 @@
 		</thead>
 		<?php
 			foreach (Brand::getBrandList() as $brand) {
-				echo '<tr>';
-					echo '<td>' . $brand->id . '</td>';
+				echo '<tr data-id="' . $brand->id . '">';
 					echo '<td>' . $brand->name . '</td>';
 					echo '<td><a href="index.php?page=admin/brand-edit&amp;id=' . $brand->id . '"><i class="fa fa-pencil" data-toggle="tooltip" title="Modifier"></i></a></td>';
-					echo '<td><a href="#"><i class="fa fa-trash" data-toggle="tooltip" title="Supprimer"></i></a></td>';
+					echo '<td><a href="#" title="Supprimer" data-action="delete" data-toggle="tooltip" title="Supprimer"><i class="fa fa-trash"></i></a></td>';
 				echo '</tr>';
 			}
 		?>
 	</table>
 </div>
+<script>
+	$('[data-action="delete"]').click(function(e) {
+		e.preventDefault();
+
+		eAjax(
+			'public/webservice/admin/brand-delete.php',
+			{'delete': true, 'id': $(this).parent().parent().data('id')},
+			'deleteRow'
+		);
+	});
+
+	var eAjaxData = '';
+
+	function eAjax(url, parameters, callback) {
+	    if (!confirm('Êtes-vous sûr ?')) {
+	        return false;
+	    }
+
+	    $.post(url, parameters, function(data) {
+	        eAjaxData = data;
+	        var func = callback + "()";
+	        eval(func);
+	    }, "json");
+	}
+
+	function deleteRow() {
+	    if (eAjaxData.status == 'true') {
+	        $('[data-id="' + eAjaxData.id + '"]').fadeTo('slow', 0.01).slideUp('slow');
+	    }
+	    
+	    else {
+	        alert(eAjaxData.status);
+	    }
+	}
+</script>
