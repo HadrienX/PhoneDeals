8bd5c71eff43d00d9c6de322258df14aa4f45879
ValGou <val.gougeon@hotmail.fr>
2016-03-15 12:27:38 +0100

    Create add-brand.php

---
+++ app/view/admin/add-brand.php
@@ -0,0 +1,48 @@
+<?php
+
+		if (isset($_POST['add']) && App::isAdmin()) {
+
+			if (isset($_POST['name']) && preg_match("#^[a-zA-Z._-]{2,32}#", $_POST['name'])){
+
+				$name = $_POST['name'];
+
+				PDOConnexion::setParameters('phonedeals', 'root', 'root');
+				$db = PDOConnexion::getInstance();
+				$sql = "
+					INSERT INTO brand(name)
+					VALUES (:name)
+				";
+				$sth = $db->prepare($sql);
+				$sth->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'Brand');
+				$sth->execute(array(
+					':name' => $name,
+				));
+			
+				if ($sth) {
+					App::success('Cette marque a bien été ajouté.','index.php?page=admin/brands-list');
+				}
+			}
+			
+			else {
+
+				App::error('Veuillez entrer une name.','index.php?page=admin/add-brand');
+
+			}
+		}
+?>
+				<div class="col-md-8">
+					<div class="page-header">
+						<h1>
+							Ajouter une Marque
+						</h1>
+					</div>
+
+					<form action="index.php?page=admin/add-brand" method="POST" enctype="multipart/form-data">
+						<div class="form-group">
+							<label for="brand-name">Nom</label>
+							<input type="text" class="form-control" id="brand-name" required="required" name="name" placeholder="Votre marque">
+						</div>
+
+						<button type="submit" class="btn btn-lg btn-primary" name="add">Ajouter</button>
+					</form>
+				</div>
