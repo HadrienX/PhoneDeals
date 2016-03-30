9f72233b041006cb5de2f51ce8cd527ecf4e4343
Hadrien <hadriien@live.fr>
2016-03-30 12:48:21 +0200

    Update cart.php
    Panier totalement fonctionnel

--- PHP/app/view/cart.php
+++ PHP/app/view/cart.php
@@ -33,8 +33,11 @@
 
 		elseif ($_POST['action'] == 'remove') {
 			$_SESSION['cart']['phone_total'] = $_SESSION['cart']['phone_total'] - $_SESSION['cart']['phones'][$_POST['id']]['amount'];
-			$_SESSION['cart']['price_total'] = $_SESSION['cart']['price_total'] - $_SESSION['cart']['phones'][$_POST['id']]['price'];
+			$_SESSION['cart']['price_total'] = $_SESSION['cart']['price_total'] - ($_SESSION['cart']['phones'][$_POST['id']]['price'] * $_SESSION['cart']['phones'][$_POST['id']]['amount']);
 			unset($_SESSION['cart']['phones'][$_POST['id']]);
+			// App::dd($_SESSION['cart']['phones']);
+
+			$msg->success('Le produit a bie été supprimé de votre panier', 'index.php?page=cart');
 		}
 
 		else {
@@ -64,22 +67,30 @@
 						<th>Prix</th>
 						<th>Couleur</th>
 						<th>Capacité</th>
-						<th>Total</th>
+						<th>Quantité</th>
+						<th>Prix total</th>
 						<th>Supprimer</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php
 						if (!$empty) {
-							foreach ($_SESSION['cart']['phones'] as $phone) {
+							foreach ($_SESSION['cart']['phones'] as $key => $phone) {
 								echo '
 									<tr>
 										<td>' . $phone['name'] . '</td>
-										<td>' . money_format('%!i', $phone['price']) . '</td>
-										<td></td>
-										<td></td>
-										<td>' . money_format('%!i', $phone['price']) . '</td>
-										<td><a href="#" data-action="remove-cart" data-id="' . $phone['id'] . '" data-price="' . $phone['price'] . '" title="Retirer du panier"><i class="fa fa-trash"></i></a></td>
+										<td>' . money_format('%!i', $phone['price']) . ' &euro;</td>
+										<td><div style="background: ' . Color::getColorById($phone['color'])->hex . '; border: 1px solid #ededed; display: inline-block; width: 15px; height: 15px; border-radius: 17px;"></div> ' . Color::getColorById($phone['color'])->name . '</td>
+										<td>' . $phone['capacity'] . ' Go</td>
+										<td>' . $phone['amount'] . '</td>
+										<td>' . money_format('%!i', $phone['price'] * $phone['amount']) . ' &euro;</td>
+										<td>
+											<form action="index.php?page=cart" method="POST">
+												<input type="hidden" name="id" value="' . $key . '">
+												<input type="hidden" name="action" value="remove">
+												<button action="submit" title="Retirer du panier" class="remove-cart" style="border: none; background: transparent;"><i class="fa fa-trash"></i></button>
+											</form>
+										</td>
 									</tr>
 								';
 							}
@@ -95,11 +106,21 @@
 				</tbody>
 			</table>
 
+			<?php if ($phonesTotal) { ?>
 			<a href="#" class="btn btn-primary">
 				<i class="fa fa-shopping-basket" style="padding-right: 7px;"></i>
 				Passer la commande
 			</a>
+			<?php } ?>
 		</div>
 	</div>
 </div>
 <?php } ?>
+
+<script>
+	$('.remove-cart').click(function() {
+		if (!confirm('Voulez vous vraiment supprimer ce téléphone de votre panier ?')) {
+			return false;
+		}
+	});
+</script>
